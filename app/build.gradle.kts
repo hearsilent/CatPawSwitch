55268850655fbad2047080c33043461c2af954bf
HearSilent <hearsilent@users.noreply.github.com>
2026-03-22 20:46:39 +0800

    chore: bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.hearsilent.android.catpawswitch.sample"
         minSdk = 24
         targetSdk = 36
-        versionCode = 100000
-        versionName = "1.0.0"
+        versionCode = 100010
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
